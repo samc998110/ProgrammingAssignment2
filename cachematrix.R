845026283cfa4cb35e63de3be93b327d9da45ef1
Sam Chen <huanjin.chen89@hotmail.com>
2020-12-12 21:52:29 +0800

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The makeCacheMatrix function creates a list of function which are anchored in an environment (the environment 
+## during execution of makeCacheMatrix). When these functions are later called 
+## during the execution of cacheSolve function, they are executed in a manner that
+## relate symbols, with priority, to objects in the said environment. 
+## Access to that environment is possible because when these functions are saved, 
+## the environment is also saved as supporting info.
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -21,7 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## access the saved environment via the functions(strapped to said environment) 
+##on list to retrieve the value of the cached "i" in that environment. If no 
+##prior data saved to "i", then sovle the input matrix and save the solution to 
+##"i" in that environment.
 
 cacheSolve <- function(x, ...) {
   i <- x$geti()

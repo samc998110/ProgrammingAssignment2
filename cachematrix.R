842ce3a0db91ee02cab4d737511d3fff6451e5a2
Sam Chen <huanjin.chen89@hotmail.com>
2020-12-12 21:13:35 +0800

    wrote two functions as instructed

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(m){
+    x <<- m
+    i <<- NULL
+  }
+  get <- function() x
+  seti <- function(invertedm) {
+    i <<- invertedm
+  }
+  geti <- function() {
+    i
+  }
+  list(set = set, get = get, seti = seti, geti = geti)
 
 }
 
@@ -11,5 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  i <- x$geti()
+  if (!is.null(i)){
+    print("cached data exist")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data,...)
+  x$seti(i)
+  i
         ## Return a matrix that is the inverse of 'x'
 }
